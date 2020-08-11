43f2232637e69359083a6c27e1ddca9e0d5b21b0
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-11 18:14:42 +0530

    cleaning

--- stringSort.php
+++ stringSort.php
@@ -7,29 +7,22 @@ $string = explode(" ",$string);
 for($k = 0;$k<3;$k++){
 
 $str = str_split($string[$k]);
-print_r($str);
-
-for($i=0;$i<count($str);$i++){
-echo $str[$i];
-}
-
 $count = count($str);
 
 for($i=0;$i<$count;$i++){
 for($j=$i+1;$j<$count;$j++){
-    //echo $str[$i],$str[$j];
+
     if($str[$i] > $str[$j]){
         $temp = $str[$i];
         $str[$i] = $str[$j];
         $str[$j] = $temp;
+            }
+        }
     }
 
-}
-}
-
 $string[$k] = implode($str);
-}
 
-print_r($string);
-echo "\n";
+}
+$string = implode($string," ");
+echo $string;
 ?>
