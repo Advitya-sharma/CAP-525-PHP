037d624f7714db41cf25c8a03b09c663a84d4aa7
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-18 17:47:53 +0530

    foreach

---
+++ array.php
@@ -0,0 +1,26 @@
+<?php
+
+$arr = array(1,2,2,3,3);
+var_dump($arr);
+
+$arr = array(1=>2,2=>3,3=>4);
+
+$arrMul = array(array(1=>2));
+
+echo $arr[2]."\n";
+var_dump($arr);
+
+foreach($arr as $k=>$a){
+    echo $k."->".$a."\n";
+}
+
+echo "\n";
+
+foreach($arrMul as $arrk=>$arrv){
+    foreach($arrv as $k=>$v){
+        echo $k."->".$v."\n";
+    } 
+}
+
+
+?>
