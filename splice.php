ed7e57e6596dbdb58bea8f14b9b7ef58d6918b74
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-08-31 21:46:26 +0530

    splice each

--- splice.php
+++ splice.php
@@ -1,6 +1,7 @@
 <?php
-$arr = array("1","2","3","4","5","6","7","8","9");
-array_splice($arr,-3,2);
+$arr = array("1","2","3","4","5","6","7","8","9","10","11");
+$arr2 = array("12",13);
+array_splice($arr,3,4,$arr2);
 
 #If length is omitted, removes everything from offset to the end of the array.
 #If offset is positive then the start of the removed portion 
