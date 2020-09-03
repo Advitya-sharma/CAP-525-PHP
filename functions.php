072dc492d1bc69aa9ced578aac2b3049dfe60f6d
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-09-03 18:08:30 +0530

    functions

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
-function sum($x,$y){
-    return $x+$y;
+function sum(int $x,int $y,int $z=50){
+    return $x+$y+$z;
 }
 
-echo sum(2,3);
+echo "the sum is ".sum(2,3);
 
+
+function add(float $a,float $b):float{
+    return $a+$b;
+}
+
+echo add(1.2,3.5)
 ?>
